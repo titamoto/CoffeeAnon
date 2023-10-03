dac91041381a57cd54f4de656b166a9915f721fc
Aleksandr Savchenko <titamoto@gmail.com>
2023-10-03 10:04:34 -0700

    switch Nav.Link to react router NavLink

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,24 +4,30 @@ import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
+import { NavLink } from "react-router-dom";
+
 
 function Header( {signedUser, setSignedUser}) {
   return (
 <>
 <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid>
-      <Navbar.Brand href="/">CoffeeAnon</Navbar.Brand>
+      <Navbar.Brand>CoffeeAnon</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
             style={{ maxHeight: '100px' }}
             navbarScroll>
-            <Nav.Link href="/">All Coffee</Nav.Link>
-            <Nav.Link href="/my-reviews" disabled>My Reviews</Nav.Link>
+            <Button variant="link" className='link-light link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover'>
+            <NavLink to="/">All Coffee</NavLink></Button>
+            <Button variant="link" className='link-light link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover'>
+            <NavLink to="/my-reviews">My Reviews</NavLink></Button>
             {!signedUser ? 
-            (<Nav.Link href="/login">Sign In</Nav.Link>) :
-            (<Nav.Link href="/logout">Sign Out</Nav.Link>)}
+            ( <Button variant="link" className='link-light link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover'>
+              <NavLink to="/login">Sign In</NavLink></Button>) :
+            (<Button variant="link" className='link-light link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover'>
+            <NavLink to="/logout">Sign Out</NavLink></Button>)}
           </Nav>
           <Form className="d-flex">
             <Form.Control
