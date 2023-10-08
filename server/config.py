975faa07a36cacb947272cf82e935aa67b37fa99
Aleksandr Savchenko <titamoto@gmail.com>
2023-10-07 22:02:16 -0700

    fix db config

--- server/config.py
+++ server/config.py
@@ -13,7 +13,7 @@ load_dotenv()
 app = Flask(__name__, static_url_path='', static_folder='../client/build', template_folder='../client/build')
 
 app.secret_key = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///app.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
